1a0ba138a961a182b716b3ac150108693e281522
DaveShedar <62851456+DaveShedar@users.noreply.github.com>
2020-06-08 16:11:24 +0300

    Some message. Here...

---
+++ src/main/java/UserService/UserService.java
@@ -0,0 +1,36 @@
+package UserService;
+
+import Dao.UserHibernateDAO;
+import User.User;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService {
+
+    private static UserService userService;
+
+    public static UserService getUserService(){
+        if(userService == null){
+            userService = new UserService();
+        }
+        return userService;
+    }
+
+    public void addUser(User user) throws SQLException{
+        UserHibernateDAO.getUserHibernateDAO().addUser(user);
+    }
+
+    public boolean updateUser(User user) throws SQLException{
+        return UserHibernateDAO.getUserHibernateDAO().updateUser(user);
+    }
+
+    public User getUserById(int id){
+        return UserHibernateDAO.getUserHibernateDAO().getUserById(id);
+    }
+    public List< User > getAllUsers(){
+        return UserHibernateDAO.getUserHibernateDAO().getAllUsers();
+    }
+    public boolean deleteUser(int id) throws SQLException{
+        return UserHibernateDAO.getUserHibernateDAO().deleteUser(id);
+    }
+}
